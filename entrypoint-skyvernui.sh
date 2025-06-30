f703c143f2ac0b5fb5b15f34eb745ac803ad81e2
Prakash Maheshwaran <73785492+Prakashmaheshwaran@users.noreply.github.com>
2025-06-29 20:45:28 -0400

    Update entrypoint-skyvernui.sh

--- entrypoint-skyvernui.sh
+++ entrypoint-skyvernui.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
 set -e
 
-# setting api key
-VITE_SKYVERN_API_KEY=$(sed -n 's/.*cred\s*=\s*"\([^"]*\)".*/\1/p' .streamlit/secrets.toml)
-export VITE_SKYVERN_API_KEY
+if [ -f .streamlit/secrets.toml ]; then
+  VITE_SKYVERN_API_KEY=$(sed -n 's/.*cred\s*=\s*"\([^"]*\)".*/\1/p' .streamlit/secrets.toml)
+  export VITE_SKYVERN_API_KEY
+else
+  echo "⚠️  .streamlit/secrets.toml not found. Skipping API key export."
+fi
+
 npm run start
-
-
